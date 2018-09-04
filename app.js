64b0c14085b1ed7af66484b733008b5258501de2
80504 <80504@LCNC16V0164.corp.lgcns.com>
2018-09-04 20:33:03 +0900

    heroku mongodb connect

--- app.js
+++ app.js
@@ -33,3 +33,14 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var mongoose    = require('mongoose');
+
+var db = mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function(){
+ console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://heroku_7rzgg5js:kjn2hj0mimru1q4uoq392jvr3e@ds143932.mlab.com:43932/heroku_7rzgg5js');
